d3e5e1d5e2409a470f4c3e61b18e43d5c14b39ed
Jesus Suarez <37440029+jsuarez1994@users.noreply.github.com>
2022-06-02 11:19:22 +0200

    Update UserDTO.java

--- dto/UserDTO.java
+++ dto/UserDTO.java
@@ -2,7 +2,6 @@ package dto;
 
 import java.util.List;
 
-import es.santander.ucrapl.ucrmonogrucr.dto.QuestionDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
